33e08cbf18d0ee2fd9de4309f9a6dd7c6a846ff6
nadaaladdin <nadaalaaedin199@gmail.com>
2023-05-12 01:05:23 +0300

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SingIn';
+import SingUp from './pages/SingUp';
+import Projects from './pages/Projects';
+import ForgotPassword from './pages/ForgotPassword';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1 >hello world! </h1>
-      </div>
+      <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/profile" element={<Profile />}/>
+          <Route path="/sign-in" element={<SignIn />}/>
+          <Route path="/sign-up" element={<SingUp />}/>
+          <Route path="/projects" element={<Projects />}/>
+          <Route path="/forgot-password" element={<ForgotPassword />}/>
+
+        </Routes>
+      </Router>
+      </>
     );
   }
 }
