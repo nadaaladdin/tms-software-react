a54026418bfa422aae3a09156c2758f507fbb5be
nadaaladdin <nadaalaaedin199@gmail.com>
2023-05-13 00:08:09 +0300

    complete the sign in functionality

--- src/pages/SingIn.jsx
+++ src/pages/SingIn.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import {AiFillEyeInvisible, AiFillEye} from "react-icons/ai"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
+import {toast} from "react-toastify"
 
 export default function SingIn() {
   const [showPassword, setShowPassword] = useState(false);
@@ -10,12 +12,25 @@ export default function SingIn() {
     password: "",
   });
   const {email, password} = formData;
-
+  const navigate =useNavigate()
   function onChange(e){
     setFormData((prevState)=>({
       ...prevState,
       [e.target.id]: e.target.value,
     }))
+  }
+  async function onSubmit(e){
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+      if(userCredential.user){
+        toast.success("Sign in was successfully")
+        navigate("/")
+      }
+    } catch (error) {
+      toast.error("Bad user credentials")
+    }
   }
   return (
     <section>
@@ -29,7 +44,7 @@ export default function SingIn() {
                 className='w-full rounded-xl'/>
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form >
+          <form onSubmit={onSubmit}>
             <input 
                 type="email"
                 id="email" 
